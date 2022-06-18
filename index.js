7892272e76998b05a30c49acc51d00bf2d9cf8d1
Sarah <asarahali00@gmail.com>
2022-06-18 14:53:33 -0500

    move all backend files into index.js

--- index.js
+++ index.js
@@ -1,15 +1,115 @@
-const {
-  getRandomGameState,
-  getEmptyBoard,
-  getEmptyHitCountsObject,
-  isEndGame,
-  getOtherPlayer,
-  validCell,
-  directions,
-} = require('./helpers');
-const {
-  Status, AttackTypes, MoveTypes, Ships,
-} = require('./types');
+const Status = Object.freeze({
+  PreGame: 0,
+  InGame: 1,
+  EndGame: 2,
+});
+
+const MoveTypes = Object.freeze({
+  InitializeBoard: 0,
+  Attack: 1,
+});
+
+const AttackTypes = Object.freeze({
+  None: 0,
+  Miss: 1,
+  Hit: 2,
+  Sunk: 3,
+});
+
+const Ships = Object.freeze({
+  Carrier: 5,
+  Battleship: 4,
+  Cruiser: 3,
+  Submarine: 3,
+  Destroyer: 2,
+});
+
+const getEmptyHitCountsObject = () => {
+  const hitCounts = { ...Ships };
+
+  Object.keys(hitCounts).forEach((ship) => {
+    hitCounts[ship] = 0;
+  });
+
+  return hitCounts;
+};
+
+const getEmptyBoard = (defaultValue) => Array
+  .from({ length: 10 }, () => Array(10)
+    .fill(defaultValue));
+
+const validCell = (x, y, board) => x >= 0 && x < board.length && y >= 0 && y < board[x].length;
+
+const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+
+const getRandomGameState = () => {
+  const getRandomValue = (max) => Math.floor(Math.random() * max);
+
+  const playerBoard = getEmptyBoard(null);
+
+  const setBoatPosition = (ship) => {
+    let startPosition = [
+      getRandomValue(playerBoard.length),
+      getRandomValue(playerBoard[0].length),
+    ];
+
+    const startDirection = getRandomValue(directions.length);
+    const shipLength = Ships[ship];
+
+    const getNextDirection = (direction) => (direction + 1) % directions.length;
+
+    const isValidStartPosition = (position, direction) => {
+      const [x, y] = direction;
+
+      for (let i = 0; i < shipLength; i += 1) {
+        const nextX = position[0] + (i * x);
+        const nextY = position[1] + (i * y);
+        if (!(
+          validCell(nextX, nextY, playerBoard) && playerBoard[nextX][nextY] === null)) {
+          return false;
+        }
+      }
+
+      return true;
+    };
+
+    let validStart = isValidStartPosition(startPosition, directions[startDirection]);
+    let currentDirection = startDirection;
+
+    while (!validStart) {
+      currentDirection = getNextDirection(currentDirection);
+
+      if (currentDirection === startDirection) {
+        startPosition = [
+          getRandomValue(playerBoard.length),
+          getRandomValue(playerBoard[0].length),
+        ];
+      }
+
+      validStart = isValidStartPosition(startPosition, directions[currentDirection]);
+    }
+
+    const [x, y] = directions[currentDirection];
+
+    for (let i = 0; i < shipLength; i += 1) {
+      const nextX = startPosition[0] + (i * x);
+      const nextY = startPosition[1] + (i * y);
+
+      playerBoard[nextX][nextY] = ship;
+    }
+  };
+
+  Object.keys(Ships).forEach((ship) => {
+    setBoatPosition(ship);
+  });
+
+  return playerBoard;
+};
+
+const isEndGame = (hitCounts) => JSON.stringify(hitCounts) === JSON.stringify(Ships);
+
+const getOtherPlayer = (players, currentPlayerID) => players
+  .find((plr) => plr.id !== currentPlayerID);
 
 /**
  * Generic board game types
